bf6644abe0d216a369da74720234f78cb38ac23a
Igor Loginov <igor.loginov@deltatre.com>
2022-04-20 20:29:58 +0200

    add table

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ function csv2json(csv) {
           dow: p[2],
           sist: parseInt(p[3]),
           diast: parseInt(p[4]),
+          pulse: parseInt(p[5]),
         });
       }
     })
@@ -28,11 +29,40 @@ function csv2json(csv) {
   return results;
 }
 
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+
+function generateTable(table, data) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (key in element) {
+      let cell = row.insertCell();
+      let val = element[key] instanceof Date ? element[key].toISOString().split('T')[0] : element[key];
+      let text = document.createTextNode(val);
+      cell.appendChild(text);
+    }
+  }
+}
+
 // `window.onload` respects `async` calls
 window.onload = async () => {
   const csv = await getData('Blood pressure.csv');
   if (csv) {
     const data = csv2json(csv);
+
+    const table = document.querySelector('table')
+    const headers = ['Date', 'AM/PM', 'Day of Week', 'Syst', 'Diast', 'Pulse']
+    generateTableHead(table, headers);
+    generateTable(table, data);
+
     const labels = data.filter(x => x.ampm === 'am').map(v => v.date.toISOString().split('T')[0]);
     const sistMorning = data.filter(x => x.ampm === 'am').map((v) => { return { x: v.date, y: v.sist }});
     const diastMorning = data.filter(x => x.ampm === 'am').map((v) => { return { x: v.date, y: v.diast }});
